5bcc1d1466415a3a2f0e86ba54ab37226cbaa0c6
Espen Sjo <sjoesp99@outlook.com>
2022-12-29 17:02:59 +0100

    refactor: profile page

--- src/Pages/ProfilePage.tsx
+++ src/Pages/ProfilePage.tsx
@@ -1,7 +1,27 @@
+import { useEffect } from "react"
 import { useParams } from "react-router-dom"
+import { GetUserProfile, GetUserProfileByUsername } from "../Common/util/API"
+import { useProfileStore } from "../Store/AuthStore"
 
 const ProfilePage = () => {
   const { username } = useParams()
+  const profileStore = useProfileStore((state) => state)
+
+  useEffect(() => {
+    const getProfile = async () => {
+      let profile
+      if (!username) {
+        return
+      }
+      if (
+        profileStore.User.username.toLowerCase() === username?.toLowerCase()
+      ) {
+      } else {
+        profile = await GetUserProfileByUsername(username)
+      }
+    }
+  }, [profileStore.User.username, username])
+
   console.log(username)
   return <div>ProfilePage</div>
 }
